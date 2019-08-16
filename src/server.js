d289ba2b4a94a6cd5e174f8773e8893d578ce6e2
Egemen Özdilek <egeoozdilek@gmail.com>
2019-08-17 01:30:18 +0300

    heroku support added

--- src/server.js
+++ src/server.js
@@ -23,5 +23,5 @@ io.on('connection', function(socket){
     });
   });  
 
-http.listen(4000);
-  console.log('listening on *:4000');
+http.listen(process.env.PORT || 4000);
+  console.log('listening');

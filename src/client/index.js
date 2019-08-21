47b5b5f4756bf14bd9827a7adf5b7a69f0dc51b6
Egemen Özdilek <egeoozdilek@gmail.com>
2019-08-22 00:05:44 +0300

    name and css added

--- src/client/index.js
+++ src/client/index.js
@@ -2,10 +2,18 @@
 var input = document.getElementById("input"); 
 var messages = document.getElementById("messages")
 
+var name = prompt("what is your name","");
+
 function send() {
-    socket.emit("send message",input.value); 
+    socket.emit("send message",({
+        name: window.name,
+        txt: input.value
+    })); 
+    input.value = "";
 }
 socket.on("msg", function(data) {
-    messages.innerHTML+= ("<br/>" + data )
+    messages.innerHTML = ( "<div class='message'> <p class = 'name'>" + data.name + ":</p>" +
+                           "<p class = 'txt' >"+ data.txt + "</p></div>" + 
+                            messages.innerHTML );
 })
 
